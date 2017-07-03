e9dad7807dc34aa697b1cca2beeb2c196efed797
Marcos Adamczuk <marcos.adamczuk@gmail.com>
2017-07-02 23:19:55 -0300

    ehhhhhhhhhhH

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ typedef struct btree_block {
 } block;
 
 typedef struct btree_promote_info {
-    int promoted_val;
+    int promoted_key;
     block * new_child;
 } promote_info;
 
@@ -26,12 +26,12 @@ int main(void) {
 
 block * initialize(void) {
     block * new_btree = (block *) malloc(sizeof(block));
-    new_btree->degree = 0;
+    new_btree->degree = 1;
 
     new_btree->keys = (int *) malloc(sizeof(int) * (2 * T  - 1));
 
-    new_btree->children = (block **) malloc(sizeof(block) * 2 * T);
-    memset(new_btree->children, 0, sizeof(block) * 2 * T); // seta os ponteiros para os filhos como NULL
+    new_btree->children = (block **) malloc(sizeof(block *) * 2 * T);
+    memset(new_btree->children, 0, sizeof(block *) * 2 * T); // seta os ponteiros para os filhos como NULL
 
     new_btree->father = NULL;
 
@@ -44,7 +44,7 @@ promote_info * _insert(block * tree, int val) {
     block * aux_tree;
     int i;
 
-    for (int i = 0; i < tree->degree - 1; i++) {
+    for (i = 0; i < tree->degree - 1; i++) {
         if (tree->keys[i] == val) return;
     }
 
@@ -58,11 +58,12 @@ promote_info * _insert(block * tree, int val) {
         if (promote_receive->new_child != NULL) {
             //TODO
             //tem que fazer promote do filho:(
-            //
-            //talvez tenha que fazer promote do atual tbm
             
             if (tree->degree == 2 * T) {
-                //tem que fazer split e promote
+                //tem que fazer split e promote deste bloco
+                //a key promovida vai ser a de índice T
+                
+
             }
             //não tem que fazer split e promote
         }
@@ -79,10 +80,10 @@ promote_info * _insert(block * tree, int val) {
         //copia todos as keys depois da key promovida para o novo nodo à direita
         for (i = T - 1; i < 2 * T - 1; i++) {
             promote->new_child->keys[i - T + 1] = tree->keys[i];
-            promote->promoted_val = tree->keys[T];
+            promote->promoted_key = tree->keys[T];
         }
 
-        if (val < promote->promoted_val) aux_tree = tree;
+        if (val < promote->promoted_key) aux_tree = tree;
         else aux_tree = promote->new_child;
 
         aux_tree->degree++;
@@ -97,8 +98,20 @@ promote_info * _insert(block * tree, int val) {
     return promote;
 }
 block * insert(block * tree, int val) {
-    promote_info * pr = _insert(tree, val); //possível raiz
-    return pr == NULL ? tree : pr;
+    promote_info * pr = _insert(tree, val); //possível nova raiz
+
+    if (pr->new_child != NULL) { //tem que criar uma nova raiz
+        block * new_root = (block *) malloc(sizeof(block));
+        block->degree = 2;
+        block->children[0] = tree;
+        block->children[1] = pr->new_child;
+        block->keys[0] = pr->promoted_key;
+
+        return new_root;
+    }
+
+    //não tem nova raiz
+    return tree;
 }
 
 void _print_tree(block * tree, int level) {
@@ -109,4 +122,4 @@ void _print_tree(block * tree, int level) {
     }
     _print_tree(tree->children[degree - 1], level + 1);
 }
-void print_tree(block * tree) { return _print_tree(tree, 0); }
+void print_tree(block * tree) { _print_tree(tree, 0); }
