cfe1d43f1a3143e091684f7d1ca91ec7f5338c56
Marcos Adamczuk <marcos.adamczuk@gmail.com>
2017-07-03 19:46:13 -0300

    getting stuff done

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int T;
+int T; //grau mínimo de um bloco
 
 typedef struct btree_block {
     int degree; // o grau do bloco
@@ -18,9 +18,33 @@ typedef struct btree_promote_info {
 
 block * initialize(void);
 block * insert(block * tree, int val);
+void print_tree(block * tree);
 
 int main(void) {
-    scanf("%d\n", &T);
+    int opcao, value;
+
+    block * tree = initialize();
+
+    printf("Valor a ser usado para T: ");
+    scanf("%d", &T);
+
+    do {
+        printf("\n1 - Inserir um elemento\n");
+        printf("2 - Listar os elementos\n");
+        printf("0 - Sair do programa\n");
+
+        scanf("%d", &opcao);
+
+        if (opcao == 1) {
+            printf("Valor a ser inserido: ");
+            scanf("%d", &value);
+
+            tree = insert(tree, value);
+        }
+        else if (opcao == 2) {
+            print_tree(tree);
+        }
+    } while (opcao);
     return 0;
 }
 
@@ -38,74 +62,118 @@ block * initialize(void) {
     return new_btree;
 }
 
-promote_info * _insert(block * tree, int val) {
+void swap(int * key_a, block ** block_a, int * key_b, block ** block_b) {
+    int key_aux;
+    block * block_aux;
+
+    key_aux = *key_a;
+    *key_a = *key_b;
+    *key_b = key_aux;
+
+    block_aux = *block_a;
+    *block_a = *block_b;
+    *block_b = block_aux;
+}
+
+void insert_in_block(block * tree, promote_info promote_receive) {
+    //só entra nesta função quando o grau de tree é menor que 2T
+    int i;
+
+    tree->keys[tree->degree - 1] = promote_receive.promoted_key;
+    tree->children[tree->degree] = promote_receive.new_child;
+
+    for (i = tree->degree; i >= 1 && tree->keys[i - 2] > tree->keys[i - 1]; i--) {
+        //se a key que está sendo inserida é menor que a anterior, elas devem ser trocadas
+        //juntamente com o ponteiro para o filho da direita
+
+        //index do filho à direita da key = i (tree->children)
+        //index da key = i - 1 (tree->keys)
+
+        swap(&tree->keys[i - 1], &tree->children[i], &tree->keys[i - 2], &tree->children[i - 1]);
+    }
+
+    tree->degree++;
+}
+
+promote_info _insert(block * tree, int val) {
 
     promote_info promote_receive, promote_send;
     block * aux_tree;
     int i;
 
-    for (i = 0; i < tree->degree - 1; i++) {
-        if (tree->keys[i] == val) return;
+    promote_send.new_child = NULL;
+
+    for (i = 0; i < tree->degree - 1; i++) { //se a key já existir, retorna um promote_info vazio
+        if (tree->keys[i] == val) {
+            return promote_send;
+        }
     }
 
     if (tree->children[0] != NULL) {
         //se este bloco não é uma folha
-        
+
         for (i = 0; i < tree->degree - 1 && val > tree->keys[i]; i++);
 
         promote_receive = _insert(tree->children[i], val);
 
-        if (promote_receive->new_child != NULL) {
+        if (promote_receive.new_child != NULL) {
             //TODO
             //tem que fazer promote do filho:(
-            
+
             if (tree->degree == 2 * T) {
                 //tem que fazer split e promote deste bloco
                 //a key promovida vai ser a de índice T
-                
+
 
             }
             //não tem que fazer split e promote
         }
 
-        promote->new_child = NULL;
-        return promote;
+        promote_send.new_child = NULL;
+        return promote_send;
     }
 
-    //se for uma folha
-    if (tree->degree == 2 * T) {
-        //vai ter que fazer promote
-        promote->new_child = initialize();
-        
+    //se for uma folha ou se a raiz não tiver filhos
+
+    if (tree->degree == 2 * T) {// vai ter que fazer promote
+        promote_send.new_child = initialize();
+
         //copia todos as keys depois da key promovida para o novo nodo à direita
         for (i = T - 1; i < 2 * T - 1; i++) {
-            promote->new_child->keys[i - T + 1] = tree->keys[i];
-            promote->promoted_key = tree->keys[T];
+            promote_send.new_child->keys[i - T + 1] = tree->keys[i];
+            promote_send.promoted_key = tree->keys[T];
         }
 
-        if (val < promote->promoted_key) aux_tree = tree;
-        else aux_tree = promote->new_child;
+        if (val < promote_send.promoted_key) aux_tree = tree;
+        else aux_tree = promote_send.new_child;
 
         aux_tree->degree++;
-        aux_tree[T - 1] = val;
+        aux_tree->keys[T - 1] = val;
 
-        return promote;
+        return promote_send;
     }
 
-    tree->children[tree->degree - 1] = val;
-    tree->degree++;
+    //não vai ter que fazer promote
 
-    return promote;
+    //printf("tree->degree - 1 = %d\n", tree->degree - 1);
+    //tree->keys[tree->degree - 1] = val;
+    //tree->degree++;
+    
+    promote_receive.promoted_key = val;
+    promote_receive.new_child = NULL;
+    insert_in_block(tree, promote_receive);
+
+    return promote_send;
 }
 block * insert(block * tree, int val) {
-    promote_info * pr = _insert(tree, val); //possível nova raiz
+    promote_info pr = _insert(tree, val); //possível nova raiz
 
-    if (pr->new_child != NULL) { //tem que criar uma nova raiz
+    if (pr.new_child != NULL) { //tem que criar uma nova raiz
         block * new_root = (block *) malloc(sizeof(block));
-        block->degree = 2;
-        block->children[0] = tree;
-        block->children[1] = pr->new_child;
-        block->keys[0] = pr->promoted_key;
+        new_root->degree = 2;
+        new_root->children[0] = tree;
+        new_root->children[1] = pr.new_child;
+        new_root->keys[0] = pr.promoted_key;
 
         return new_root;
     }
@@ -117,9 +185,9 @@ block * insert(block * tree, int val) {
 void _print_tree(block * tree, int level) {
     if (tree == NULL) return;
     for (int i = 0; i < tree->degree - 1; i++) {
-        print_tree(tree->children[i], level + 1);
-        printf("%d\n", tree->val[i]);
+        _print_tree(tree->children[i], level + 1);
+        printf("%d\n", tree->keys[i]);
     }
-    _print_tree(tree->children[degree - 1], level + 1);
+    _print_tree(tree->children[tree->degree - 1], level + 1);
 }
 void print_tree(block * tree) { _print_tree(tree, 0); }
